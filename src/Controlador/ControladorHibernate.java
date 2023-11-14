772805549e631078127720a6d544a06a765cdaa2
DDargn <117987308+DDargn@users.noreply.github.com>
2023-11-14 19:02:15 +0100

    Controlador comentado

--- src/Controlador/ControladorHibernate.java
+++ src/Controlador/ControladorHibernate.java
@@ -15,24 +15,23 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-/**
- *
- * @author usuario
- */
 public class ControladorHibernate {
 
+    //Instanciamos el Session Factory
     private SessionFactory instancia;
     private Session sesion;
 
+    //Creamos el Constructor de la clase
     public ControladorHibernate() {
         instancia = (SessionFactory) new Configuration().configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Doctor.class)
                 .addAnnotatedClass(Especialidad.class)
                 .addAnnotatedClass(Usuario.class)
                 .buildSessionFactory();
-        
+
     }
 
+    //Creamos el metodo Instertar
     public void insertar(Object objeto) {
         sesion = instancia.openSession();
         sesion.beginTransaction();
@@ -40,48 +39,51 @@ public class ControladorHibernate {
         sesion.getTransaction().commit();
         sesion.close();
     }
-    
-    public void delete (Object objeto){
+
+    //Creamos el metodo Delete
+    public void delete(Object objeto) {
         sesion = instancia.openSession();
         sesion.beginTransaction();
         sesion.delete(objeto);
         sesion.getTransaction().commit();
         sesion.close();
     }
-    
-    public void update (Object objeto){
+
+    //Creamos el metodo Update
+    public void update(Object objeto) {
         sesion = instancia.openSession();
         sesion.beginTransaction();
         sesion.update(objeto);
         sesion.getTransaction().commit();
         sesion.close();
     }
-    
-    public ArrayList<Object> getDatos(Class<?> clase){
+
+    //Creamos una consulta select 
+    public ArrayList<Object> getDatos(Class<?> clase) {
         sesion = instancia.openSession();
         sesion.beginTransaction();
-        
+
         String hql = "FROM " + clase.getSimpleName().toLowerCase();
-        
-        Query<Object> consulta =(Query<Object>) sesion.createQuery(hql,clase);
+
+        Query<Object> consulta = (Query<Object>) sesion.createQuery(hql, clase);
         List<Object> result = consulta.getResultList();
         sesion.close();
-        
+
         return (ArrayList<Object>) result;
     }
-    
-    public ArrayList<Object> getDatosWhere(Class<?> clase, String condicion, String valor){
+
+    //Creamos una consulta select Where
+    public ArrayList<Object> getDatosWhere(Class<?> clase, String condicion, String valor) {
         sesion = instancia.openSession();
         sesion.beginTransaction();
-        
+
         String hql = "FROM " + clase.getSimpleName().toLowerCase() + " WHERE " + condicion + " =: " + valor;
-        
-        
+
         Query<Object> consulta = (Query<Object>) sesion.createQuery(hql, clase);
         consulta.setParameter(condicion, valor);
         List<Object> result = consulta.getResultList();
         sesion.close();
-        
+
         return (ArrayList<Object>) result;
     }
 }
