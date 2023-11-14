f338c9ad260a6c6bacbf052523110a458507f5e9
Francisco Ruiz Nieto <franciscoruiznietoo@gmail.com>
2023-11-14 09:19:13 +0100

    Clase usuario

---
+++ src/Modelo/Usuario.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author usuario
+ */
+@Entity
+@Table (name = "usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "idusuario")
+    private String idusuario;
+    @Column (name = "pass")
+    private String pass;
+    @Column (name = "rol")
+    private int rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(String idusuario, String pass, int rol) {
+        this.idusuario = idusuario;
+        this.pass = pass;
+        this.rol = rol;
+    }
+
+    public String getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(String idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "idusuario=" + idusuario + ", pass=" + pass + ", rol=" + rol + '}';
+    }
+}
